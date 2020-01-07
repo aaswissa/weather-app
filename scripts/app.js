d5e8d0394eaaa9d8bd5b82cc66bbe98dc67f076a
Aviran Avraham Swissa <aviran.swissa@gmail.com>
2020-01-07 20:39:58 +0200

    first weather-app commit

---
+++ scripts/app.js
@@ -0,0 +1,74 @@
+const cityForm = document.querySelector('form')
+const card = document.querySelector('.card')
+const details = document.querySelector('.details')
+const time = document.querySelector('img.time')
+const icon = document.querySelector('.icon img')
+const forecast = new Forecast()
+
+const updateUI = (data) => {
+
+  const { location, weather } = data
+  
+  details.innerHTML = 
+    `<h5 class="my-3">${location.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+    <span>${Math.round(weather.Temperature.Metric.Value)}</span>
+    <span>&deg;C</span>
+    </div>`
+
+    console.log(card);
+    
+  const iconSource = `img/icons/${weather.WeatherIcon}.svg`
+  icon.setAttribute('src', iconSource)
+
+  let timeSource = null
+  weather.IsDayTime ? timeSource = 'img/day.svg' : timeSource = 'img/night.svg'
+  time.setAttribute('src', timeSource)
+
+  card.classList.contains('d-none') ? card.classList.remove('d-none') : null
+}
+
+cityForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const city = cityForm.city.value.trim()
+  cityForm.reset()
+
+  localStorage.city = city
+
+  forecast.updateCity(city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err))
+})
+
+if (localStorage.city) {
+  forecast.updateCity(localStorage.city)
+    .then((data) => updateUI(data))
+    .catch((err) => console.log(err))
+}
+
+forecast.getCity()
+  .then((data) => {
+    return forecast.getWeather(data.Key)
+  })
+  .then((data) => console.log(data))
+  .catch((err) => console.log('rejected', err.message))
+
+
+  function algosearch() {
+    places({
+      appId: 'pl2VD406Q1Z4',
+      apiKey: '18797bd8e1b23beed2316c561b2458f7',
+      container: document.querySelector('#city'),
+      templates: {
+        value: function(suggestion) {
+          return suggestion.name;
+        }
+      }
+    }).configure({
+      type: 'city',
+      aroundLatLngViaIP: false,
+    });
+  }
+  algosearch();
---
+++ scripts/forecast.js
@@ -0,0 +1,33 @@
+class Forecast {
+  constructor() {
+    this.key = 'NuAzhMhDfpJgiwOu3MWcAVwAahI4iHWJ'
+    this.weatherURI = 'https://dataservice.accuweather.com/currentconditions/v1/'
+    this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search'
+  }
+  async updateCity(city) {
+    const location = await this.getCity(city)
+    const weather = await this.getWeather(location.Key)
+  
+    return { location, weather }
+  }
+  async getCity(city) {
+    const base = this.cityURI
+    const query = `?apikey=${this.key}&q=${city}`
+  
+    const response = await fetch(base + query)
+    const data = await response.json()
+  
+    return data[0]
+  }
+  async getWeather(city) {
+    const base = this.weatherURI
+    const query = `${city}?apikey=${this.key}`
+  
+    const response = await fetch(base + query)
+    const data = await response.json()
+  
+    return data[0]
+  }
+}
+
+
